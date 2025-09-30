4cae5e2da46564b3f4494bdea639e979294fa1f5
NirajTharu <nirajc279@gmail.com>
2025-09-30 15:31:19 +0545

    Dice to get the random number while using it

---
+++ Dice_Random/Program.cs
@@ -0,0 +1,14 @@
+﻿// To get the random dice number from using the random functionn in c#
+using System;
+namespace Dice_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int roll = dice.Next(1, 7);
+            Console.WriteLine(roll);
+        }
+    }
+}
