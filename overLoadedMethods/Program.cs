09d612801e641b2d3e4aa9f5b192d0f02b93539b
NirajTharu <nirajc279@gmail.com>
2025-09-30 16:42:25 +0545

    About the overoaded methods in c#

---
+++ overLoadedMethods/Program.cs
@@ -0,0 +1,21 @@
+﻿// Overloaded Methods
+
+using System;
+namespace OverloadedMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int roll1 = dice.Next();
+            int roll2 = dice.Next(101);
+            int roll3 = dice.Next(50, 101);
+
+            Console.WriteLine($"Frist Roll:{roll1}");
+            Console.WriteLine($"Second Roll:{roll2}");
+            Console.WriteLine($"Third Roll:{roll3}");
+            
+        }
+    }
+}
