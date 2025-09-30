d33ab4487e1fb60d12c9c2fc6eb848e21d0c0889
NirajTharu <nirajc279@gmail.com>
2025-09-30 20:48:57 +0545

    Game with various conditions and bonuses

--- Dice_Game/Program.cs
+++ Dice_Game/Program.cs
@@ -4,28 +4,33 @@
 
 
 using System;
-namespace DiceGame
+Random dice = new Random();
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (roll1 == roll2 && roll2 == roll3)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Random dice = new Random();
-            int roll1 = dice.Next(1, 7);
-            int roll2 = dice.Next(1, 7);
-            int roll3 = dice.Next(1, 7);
+    Console.WriteLine("You rolled triples! + 6 bonus to total!");
+    total += 6;
+    Console.WriteLine($"Your total is {total}");
+}
+else if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! + 2 bonus to total!");
+    total += 2;
+    Console.WriteLine($"Your total is {total}");
+}
 
-            int total = roll1 + roll2 + roll3;
-
-            Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-            if (total > 14)
-            {
-                Console.WriteLine("You have won the Game");
-            }
-            if (total < 15)
-            {
-                Console.WriteLine("You Lose");
-            }
-        }
-    }
+if (total >= 15)
+{
+    Console.WriteLine("You have won the Game");
+}
+if (total < 15)
+{
+    Console.WriteLine("You Lose");
 }
