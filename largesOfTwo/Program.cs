de1826102c508884ff5155a23643eb7838e5fc7e
NirajTharu <nirajc279@gmail.com>
2025-09-30 16:55:55 +0545

    Adding the largest of two number folder

---
+++ largesOfTwo/Program.cs
@@ -0,0 +1,18 @@
+﻿// Implement a method of the Math class that returns the larger of two numbers
+
+using System;
+namespace LargestOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstValue = 500;
+            int secondValue = 600;
+            int largestValue;
+
+            largestValue = Math.Max(firstValue, secondValue);
+            Console.WriteLine(largestValue);
+        }
+    }
+}
