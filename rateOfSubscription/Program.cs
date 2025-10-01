f4fb758fd3d1063733fe4eca5cd29db4df5276c8
NirajTharu <nirajc279@gmail.com>
2025-10-01 18:26:22 +0545

    Adding the comments in the rate of Subscriprtion folder

--- rateOfSubscription/Program.cs
+++ rateOfSubscription/Program.cs
@@ -1,4 +1,25 @@
-﻿// 
+﻿/* Rule 1: Your code should only display one message.
+
+The message that your code displays will depend on the other five rules. For rules 2-6, the higher numbered rules take precedence over the lower numbered rules.
+
+Rule 2: If the user's subscription will expire in 10 days or less, display the message:
+Your subscription will expire soon. Renew now!
+
+Rule 3: If the user's subscription will expire in five days or less, display the messages:
+Your subscription expires in _ days.
+Renew now and save 10%!
+Be sure to replace the _ character displayed in the message above with the value stored in the variable daysUntilExpiration when you construct your message output.
+
+
+Rule 4: If the user's subscription will expire in one day, display the messages:
+
+Your subscription expires within a day!
+Renew now and save 20%!
+
+Rule 5: If the user's subscription has expired, display the message:
+Your subscription has expired.
+
+*/
 
 using System;
 namespace RateOfSubscription
