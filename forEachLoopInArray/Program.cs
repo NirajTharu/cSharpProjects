6c23461fc595bbab5fedc3acad5fa02700137fbf
NirajTharu <nirajc279@gmail.com>
2025-10-01 19:15:57 +0545

    Adding the for each loop data to the git hub

---
+++ forEachLoopInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿// foreach Loop in the array
+using System;
+namespace ForEachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This array is of the String Datatype.");
+            string[] names = ["Rowena", "Robin", "Bao"];
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            int sum = 0;
+            int bin = 0;
+            Console.WriteLine("This array is of the intiger datatype:");
+            int[] inventery = { 200, 450, 700, 175, 250 };
+            foreach (int items in inventery)
+            {
+                // Console.Write(items);
+                sum += items;
+                bin++;
+                Console.WriteLine($"Bin {bin} = {items} item (Runnint Total: {sum})");
+            }
+            Console.WriteLine($"The sum of the items is: {sum}");
+        }
+    }
+}
