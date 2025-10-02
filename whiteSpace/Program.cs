f403602a8578c5885dfc08ab1819c0dd2f371803
NirajTharu <nirajc279@gmail.com>
2025-10-02 18:43:23 +0545

    Adding the white space file

---
+++ whiteSpace/Program.cs
@@ -0,0 +1,40 @@
+﻿// About the white space and its use 
+
+using System;
+namespace WhiteSpace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.
+            // WriteLine
+            // (
+            //     "Hi there"
+            // );
+
+            Random dice = new Random();
+
+            int roll1 = dice.Next(1, 7);
+            int roll2 = dice.Next(1, 7);
+            int roll3 = dice.Next(1, 7);
+
+            int total = roll1 + roll2 + roll3;
+            Console.WriteLine($"The total is: {roll1} + {roll2} + {roll3} = {total}");
+
+            if (roll1 == roll2 || roll2 == roll3 || roll1 == roll3)
+            {
+                if (roll1 == roll2 && roll2 == roll3)
+                {
+                    Console.WriteLine("Your rolled triples ! + 6 bonus");
+                    total += 6;
+                }
+                else
+                {
+                    Console.Write("You rolled bouble ! + 2 bonus!");
+                    total += 2;
+                }
+            }
+        }
+    }
+}
