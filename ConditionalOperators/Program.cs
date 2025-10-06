52427e9ac8987c760b25674a99da8d8ff9ec2996
NirajTharu <nirajc279@gmail.com>
2025-10-06 17:09:05 +0545

    Adding the conditional file

---
+++ ConditionalOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿
+// Conditional Operator
+using System;
+namespace ConditionlOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int salesAmount = 1000;
+            // int discount = salesAmount > 1000 ? 100 : 50;
+            // Console.WriteLine($"The discount Amount is: {discount}");
+            Console.WriteLine($"The discount amount is: {(salesAmount > 1000 ? 100 : 50)}");
+        }
+    }
+}
