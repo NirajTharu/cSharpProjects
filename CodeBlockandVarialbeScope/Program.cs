b5f070a55567c357b4eb20dade0b160195a78932
NirajTharu <nirajc279@gmail.com>
2025-10-06 19:50:25 +0545

    Adding the file of the code block and variable scope

---
+++ CodeBlockandVarialbeScope/Program.cs
@@ -0,0 +1,20 @@
+﻿// Exercise about the Code block and variable scope
+using System;
+namespace CodeBlockAndVariableScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int value = 0;
+            if (flag)
+            {
+
+                Console.WriteLine($"Inside the code block: {value}");
+            }
+            value = 11;
+            Console.WriteLine($"Outside the code block: {value}");
+        }
+    }
+}
