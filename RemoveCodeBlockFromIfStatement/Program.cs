d930d92abf9c29cdc4253bba6dee0d1d7044b857
NirajTharu <nirajc279@gmail.com>
2025-10-06 20:25:55 +0545

    Adding the Remove code block if it improves the readability of your code

---
+++ RemoveCodeBlockFromIfStatement/Program.cs
@@ -0,0 +1,36 @@
+﻿// Removing the code block form the if Statement 
+
+using System;
+namespace RemvoeBlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            // if (flag)
+            // {
+            //     Console.WriteLine(flag);
+            // }
+
+            // if (flag)
+            //     Console.WriteLine(flag);
+
+            if (flag) Console.WriteLine(flag);
+
+            string name = "steve";
+            if (name == "bob") Console.WriteLine("Found Bob");
+            else if (name == "steve") Console.WriteLine("Found Steve");
+            else Console.WriteLine("Found Chuck");
+
+            // 
+            
+            // if (name == "bob")
+            //     Console.WriteLine("Found Bob");
+            // else if (name == "steve")
+            //     Console.WriteLine("Found Steve");
+            // else
+            //     Console.WriteLine("Found Chuck");
+        }
+    }
+}
