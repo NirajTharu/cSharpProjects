a6b737b235c1f7cde98367c61985fdc9628644ed
NirajTharu <nirajc279@gmail.com>
2025-10-08 14:04:06 +0545

    Adding the foreach loop and its limitations

---
+++ ForEachLoopAndItsLimitations/Program.cs
@@ -0,0 +1,25 @@
+﻿// For each loop and its limitations
+using System;
+namespace ForEachloopAndItsLimitations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Alex", "Eddie", "David", "Michael" };
+            // foreach (var name in names)
+            // {
+            //     // LimitationOfForEachLoop
+            //     // if (name = "Alex") name = "Sammy";
+            //     Console.WriteLine(name);
+            // }
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == "Alex") names[i] = "Sammy";
+            }
+            foreach (var name in names)
+                Console.WriteLine(name);
+
+        }
+    }
+}
