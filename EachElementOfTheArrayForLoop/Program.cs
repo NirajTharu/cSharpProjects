e4687a6d74da6f2623097de89bf5a2340b136d41
NirajTharu <nirajc279@gmail.com>
2025-10-08 13:03:37 +0545

    Adding the for loop and its use

---
+++ EachElementOfTheArrayForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿// Loop through the each element of the array
+using System;
+namespace LoopThroughEachElementOfTheArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = { "Alex", "Eddie", "David", "Michael" };
+            // BackWard iteration
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(name[i]);
+            }
+            // Forward iteration
+            Console.WriteLine("For the forward iteration:");
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }
+    }
+}
