59ff0e243c283c3d9df4f9eac8f8a6b482d7eb3f
NirajTharu <nirajc279@gmail.com>
2025-10-08 12:51:46 +0545

    Adding the Basics of the for loop in the C#

---
+++ BasicsOfForLoop/Program.cs
@@ -0,0 +1,24 @@
+﻿// Basics of for loop
+using System;
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                if (i == 7) break;
+            }
+            for (int i = 0; i < 10; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
