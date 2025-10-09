af41c2c10e8b08c4984a230516bc70bf5d67b986
NirajTharu <nirajc279@gmail.com>
2025-10-09 20:01:04 +0545

    Adding the while loop file

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿// While loop
+
+using System;
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random current = new Random();
+            int value = current.Next(1, 56);
+            while (value >= 7)
+            {
+                Console.WriteLine(value);
+                value = current.Next(1, 55);
+            }
+            Console.WriteLine($"The value of the value is:{value}");
+        }
+    }
+}
