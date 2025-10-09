05fd51549adf01159b295dc71956bec87e662551
NirajTharu <nirajc279@gmail.com>
2025-10-09 19:47:26 +0545

    Adding the do while loop code

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿// Do while loop
+using System;
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int current = 0;
+            do
+            {
+                current = random.Next(1, 11);
+                Console.WriteLine(current);
+            } while (current != 7);
+        }
+    }
+}
