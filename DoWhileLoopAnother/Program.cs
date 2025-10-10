2e222fa78c79a3f907818a1349c71f2d67c4416e
NirajTharu <nirajc279@gmail.com>
2025-10-10 21:03:06 +0545

    Adding the do while loop another file

---
+++ DoWhileLoopAnother/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//  Do while loop
+using System;
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int current = random.Next(1, 11);
+            do
+            {
+                current = random.Next(1, 11);
+                if (current >= 8)
+                    continue;
+                Console.WriteLine(current);
+            } while (current != 7);
+        }
+    }
+}
