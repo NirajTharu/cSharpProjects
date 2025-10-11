c8dced623c8f51f0724acd994b4f33cec0f0a599
NirajTharu <nirajc279@gmail.com>
2025-10-11 14:05:33 +0545

    Adding the Do while loop with the at least theree character.

---
+++ NullAbleValue/Program.cs
@@ -0,0 +1,29 @@
+﻿// Using the do while loop for the user input
+using System;
+namespace NullAbleValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? inputString;
+            bool validEntry = false;
+            Console.WriteLine("Enter the string contaning at least 3 character:");
+            do
+            {
+                inputString = Console.ReadLine();
+                if (inputString != null)
+                {
+                    if (inputString.Length >= 3)
+                    {
+                        validEntry = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your input is invalid try again later.");
+                    }
+                }
+            } while (validEntry == false);
+        }
+    }
+}
