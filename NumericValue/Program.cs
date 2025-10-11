b607d8326f920dff47b3b8721e2afc158663d493
NirajTharu <nirajc279@gmail.com>
2025-10-11 14:15:26 +0545

    Adding the Numeric value file

---
+++ NumericValue/Program.cs
@@ -0,0 +1,21 @@
+﻿// To check the value is numeric or not
+using System;
+namespace NumberiValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? inputString;
+            int numericValue = 0;
+            bool validNumber = false;
+
+            Console.WriteLine("Enter the Numeric Value:");
+            inputString = Console.ReadLine();
+
+            validNumber = int.TryParse(inputString, out numericValue);
+            Console.WriteLine($"The value is numeric: {validNumber}");
+
+        }
+    }
+}
