cf3840843f8fddc965b059b61e35ab337438794c
NirajTharu <nirajc279@gmail.com>
2025-10-13 18:53:37 +0545

    Adding the UnsignedIntegral Value

---
+++ DataTypeUnsignedIntegralValue/Program.cs
@@ -0,0 +1,17 @@
+﻿// Integral value of unSigned integral data
+using System;
+namespace UnsingedIntegral
+{
+    class Progarm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unsigned Integral Types:");
+
+            Console.WriteLine($"byte {byte.MinValue} to {byte.MaxValue} ");
+            Console.WriteLine($"ushort{ushort.MinValue} to {ushort.MaxValue} ");
+            Console.WriteLine($"uint {uint.MinValue} to {uint.MaxValue} ");
+            Console.WriteLine($"ulong {ulong.MinValue} to {ulong.MaxValue} ");
+        }
+    }
+}
