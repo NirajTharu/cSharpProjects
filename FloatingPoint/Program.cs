985ab6ba4f6e84e4a871c73a6953a7eb9ffecfa0
NirajTharu <nirajc279@gmail.com>
2025-10-13 19:39:03 +0545

    Adding the float file

---
+++ FloatingPoint/Program.cs
@@ -0,0 +1,21 @@
+﻿/* When you need a more precise answer, you should use decimal. 
+Each value of type decimal has a relatively large memory footprint, 
+however performing math operations gives you a more precise result. 
+So, you should use decimal when working with financial data or 
+any scenario where you need an accurate result from a calculation.*/
+
+using System;
+namespace FloatingPointData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Floating point types:");
+            Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+            Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+            Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+        }
+    }
+}
