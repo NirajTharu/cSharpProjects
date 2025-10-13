c2c7722304970169203129a84cc202f9563b8977
NirajTharu <nirajc279@gmail.com>
2025-10-13 18:44:15 +0545

    Adding the DataTypeIntegral  file

---
+++ DataTypeIntegral/Program.cs
@@ -0,0 +1,17 @@
+﻿// About the ingegral data type
+using System;
+namespace IntegralDataType
+{
+    class Progarm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Signed integral types: ");
+
+            Console.WriteLine($"sbyte {sbyte.MinValue} to {sbyte.MaxValue}");
+            Console.WriteLine($"short {short.MinValue} to {short.MaxValue}");
+            Console.WriteLine($"int {int.MinValue} to {int.MaxValue}");
+            Console.WriteLine($"long {long.MinValue} to {long.MaxValue}");
+        }
+    }
+}
