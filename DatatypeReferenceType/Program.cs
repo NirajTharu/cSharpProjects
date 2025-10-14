c0bed55a80fa69af5da0d9f888d19f7b8b9d93fb
NirajTharu <nirajc279@gmail.com>
2025-10-14 20:25:02 +0545

    Adding a file

---
+++ DatatypeReferenceType/Program.cs
@@ -0,0 +1,16 @@
+﻿// Data type references
+using System;
+namespace RefrencesDataType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] Data;
+            // Data = new int[3];
+
+            int[] Data = new int[3];
+            Console.WriteLine(Data);
+        }
+    }
+}
