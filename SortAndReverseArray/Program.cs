d9368ad8037e67dc06fd892b1298908a0b46433e
NirajTharu <nirajc279@gmail.com>
2025-10-15 19:21:38 +0545

    Adding the file of the Sorted and reverse array

---
+++ SortAndReverseArray/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Reverse and sort an array
+using System;
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pallets = ["B14", "A11", "B12", "A13"];
+            Console.WriteLine("--Sorted Array--");
+            Array.Sort(pallets);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine(pallet);
+            }
+
+            // Reversing the array
+            Console.WriteLine("--The Reverse Array--");
+            Array.Reverse(pallets);
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine(pallet);
+            }
+        }
+    }
+}
