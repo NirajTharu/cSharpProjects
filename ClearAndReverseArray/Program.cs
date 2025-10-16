0b3cadbc708ac0a163f460634d7af58a34031bef
NirajTharu <nirajc279@gmail.com>
2025-10-16 20:29:56 +0545

    Some edits are pending

---
+++ ClearAndReverseArray/Program.cs
@@ -0,0 +1,24 @@
+﻿// Use array methods to clear and resize an array
+
+using System;
+namespace ClearAndReverseMethods
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string[] pallets = ["B14", "A11", "B12", "A13"];
+
+            Console.WriteLine($"Before:-- {pallets[0]}");
+            Array.Clear(pallets, 0, 2);
+            Console.WriteLine($"After:-- {pallets[0]}");
+
+
+            Console.WriteLine($"Clearing 2 ... Count: {pallets.Length}");
+            foreach (var pallet in pallets)
+            {
+                Console.WriteLine($"-- {pallet}");
+            }
+        }
+    }
+}
