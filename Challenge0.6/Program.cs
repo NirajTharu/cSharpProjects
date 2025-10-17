1943d0595dd82564e6e46d3725a1d2dc82e602c8
NirajTharu <nirajc279@gmail.com>
2025-10-17 19:55:45 +0545

    Adding the file of the challenge of 0.6

---
+++ Challenge0.6/Program.cs
@@ -0,0 +1,21 @@
+﻿// Challenge
+using System;
+namespace ReverseWordsInSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pangrams = "The quick brown fox jump over the lazy dog";
+            string[] SplitedStrings = pangrams.Split(' ');
+            for (int i = 0; i < SplitedStrings.Length; i++)
+            {
+                char[] valuePangrams = SplitedStrings[i].ToCharArray();
+                Array.Reverse(valuePangrams);
+                SplitedStrings[i] = new string(valuePangrams);
+            }
+            string JoinedStrings = String.Join(" ", SplitedStrings);
+            Console.WriteLine(JoinedStrings);
+        }
+    }
+}
