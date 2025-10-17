5dee018c8fd111bea23b23d30f3992cbd180ec32
NirajTharu <nirajc279@gmail.com>
2025-10-17 15:37:15 +0545

    Adding the file of the Split and join function and its use

---
+++ SplitAndJoin/Program.cs
@@ -0,0 +1,35 @@
+﻿// Split and join operations
+using System;
+using System.Diagnostics;
+namespace SplitAndJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "abc123";
+            char[] valueArray = value.ToCharArray();
+            Console.WriteLine(valueArray);
+            Array.Reverse(valueArray);
+            string result = new string(valueArray);
+            Console.WriteLine(result);
+
+            // Dividing the string by characters with a comma(,)
+            string book = "BookCoverIsLookingGood";
+            char[] wordsOfBook = book.ToCharArray();
+            Array.Reverse(wordsOfBook);
+
+            // first Final part
+            string bookResult = String.Join(",", wordsOfBook);
+            Console.WriteLine(bookResult);
+
+            // second Final part
+            // if Wanted the to display the string with out comma
+            string[] items = bookResult.Split(",");
+            foreach (string item in items)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
