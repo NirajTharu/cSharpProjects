c0ee9f4a7d3a4db5330e8af422cb2b7dadaea857
NirajTharu <nirajc279@gmail.com>
2025-10-18 07:00:47 +0545

    Adding the string formating file

---
+++ StringFormatingBasics/Program.cs
@@ -0,0 +1,17 @@
+﻿// About the string formatting basics in C#
+using System;
+namespace StringFromatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Composite formatting uses numbered placeholders within a string. 
+           At run time, everything inside the braces is resolved to a value that is also passed in based on their position.*/
+            string first = "Hello";
+            string second = "world";
+            string result = string.Format("{0} {1}!", first, second);
+            Console.WriteLine(result);
+        }
+    }
+}
