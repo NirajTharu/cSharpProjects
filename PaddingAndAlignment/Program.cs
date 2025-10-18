a5e064b76c7e2484ea3c9505ccd4170ea55f5698
NirajTharu <nirajc279@gmail.com>
2025-10-18 22:05:32 +0545

    Adding the Padding and alignment file

---
+++ PaddingAndAlignment/Program.cs
@@ -0,0 +1,25 @@
+﻿// Discovering padding and alignmnet in the C#
+using System;
+namespace PaddingAndAlignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Padding in the C# just make the given string of the input length
+            string first = "Hi";
+            Console.WriteLine(first.PadLeft(3, '*'));
+
+            // Real use case of the padding
+            string paymentID = "769C";
+            string payeeName = "Mr. Stephen Strange";
+            string paymentAmount = "$4,000,837";
+            var foramttedLine = paymentID.PadLeft(6, '-');
+            foramttedLine += payeeName.PadLeft(24);
+            foramttedLine += paymentAmount.PadLeft(14);
+            Console.WriteLine("12345678901234567890123456789012345678901234");
+            Console.WriteLine(foramttedLine);
+
+        }
+    }
+}
