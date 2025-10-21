cfcbd4866d519c2092d50488a28f9ba44cd8b12f
NirajTharu <nirajc279@gmail.com>
2025-10-21 18:18:35 +0545

    Adding the function file of the Remove and Replace functions

--- RemoveAndReplace/Program.cs
+++ RemoveAndReplace/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// The remove and replace functions in this file
+using System;
+namespace RemoveAndReplaceFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "12345John Smith          5000  3  ";
+            string updatedData = data.Remove(5, 20);
+            Console.WriteLine(updatedData);
+
+            string message = "This--is--ex-amp-le--da-ta";
+            message = message.Replace("--", " ");
+            message = message.Replace("-", "");
+            Console.WriteLine(message);
+        }
+    }
+}
