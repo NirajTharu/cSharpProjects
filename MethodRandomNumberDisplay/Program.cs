aefa0680bc6678d733cbe3a55fd3c470f71ac11b
NirajTharu <nirajc279@gmail.com>
2025-10-24 19:00:38 +0545

    Adding the first file of the Method Building

---
+++ MethodRandomNumberDisplay/Program.cs
@@ -0,0 +1,23 @@
+﻿// Building the method to display the random number
+using System;
+using System.Diagnostics;
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generating random Numbers:");
+            DisplayRandomNumbers();
+            void DisplayRandomNumbers()
+            {
+                Random random = new Random();
+                for (int i = 0; i < 5; i++)
+                {
+                    Console.Write($"{random.Next(1, 50)} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
