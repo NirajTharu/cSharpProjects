70db0b44074d92aecf5b5890de89de60884bedb6
NirajTharu <nirajc279@gmail.com>
2025-10-26 18:24:49 +0545

    Adding the file of the Parameter In the Methods

---
+++ ParameterInMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿// Using the parameter in the Methods
+using System;
+namespace ParametersInMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountTo(5);
+            void CountTo(int max)
+            {
+                for (int i = 0; i < max; i++)
+                {
+                    Console.Write($"{i}\t");
+                }
+            }
+        }
+    }
+}
