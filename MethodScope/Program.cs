bf656b9bc355073a02667775e7aeac9ad0105e41
NirajTharu <nirajc279@gmail.com>
2025-10-27 08:39:31 +0545

    Adding the file of the MethodScope file with the comments

---
+++ MethodScope/Program.cs
@@ -0,0 +1,46 @@
+﻿// Understandin the Method scope
+// Methods can not access the vraiable defined inside the another method.
+using System;
+using System.Net.NetworkInformation;
+namespace MethodScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = { "Jeena", "Ayesha", "Carlos", "Viktor" };
+            DisplayStudents(students);
+            DisplayStudents(new string[] { "Roberto", "Vaniya" });
+
+            void DisplayStudents(string[] students)
+            {
+                foreach (string student in students)
+                {
+                    Console.Write($"{student}, ");
+                }
+                Console.WriteLine();
+            }
+
+
+            double pi = 3.14159;
+            CirlceInfo(12);
+            CirlceInfo(24);
+            void PrintCircleArea(int radius)
+            {
+                double area = pi * (radius * radius);
+                Console.WriteLine($"The area of the circle is {area}");
+            }
+            void PrintCircleCircumfrence(int radius)
+            {
+                double Circumfrance = 2 * pi * radius;
+                Console.WriteLine($"Circumfrance of the Circle {Circumfrance}");
+            }
+            void CirlceInfo(int radius)
+            {
+                Console.WriteLine($"Circle with radus {radius}");
+                PrintCircleArea(radius);
+                PrintCircleCircumfrence(radius);
+            }
+        }
+    }
+}
