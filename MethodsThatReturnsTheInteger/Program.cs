59c2366c90e5f3a4a9c326d5cb514212db6c17d6
NirajTharu <nirajc279@gmail.com>
2025-10-28 12:45:22 +0545

    Adding the file of the money exchange

---
+++ MethodsThatReturnsTheInteger/Program.cs
@@ -0,0 +1,22 @@
+﻿// Methods that returns an integer 
+using System;
+namespace IntegerReturn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // $ 1 united states dollar is equal to 0.0071 Nepali Rupee
+            // Rs 141.17 = $ 1
+            double NRP = 111000;
+            double USD = NRPtoUSD(NRP);
+            Console.WriteLine($"NRP {NRP} = $ {USD} ");
+
+            double NRPtoUSD(double NRP)
+            {
+                double rate = 0.0071;
+                return (double)(rate * NRP);
+            }
+        }
+    }
+}
