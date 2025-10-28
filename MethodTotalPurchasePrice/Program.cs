ab0ca2c54b183e2a97b72940190b302529b5c72d
NirajTharu <nirajc279@gmail.com>
2025-10-28 11:57:47 +0545

    Adding the file of the total pruchase price after discount

---
+++ MethodTotalPurchasePrice/Program.cs
@@ -0,0 +1,38 @@
+﻿// This program contains the Method which helps to calculate the total price of the certain purchase
+using System;
+namespace TotalPurchasePrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0.00;
+            double minimumSpend = 30.00;
+            double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
+            double[] discount = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                total += GetDiscountPrice(i);
+            }
+            if (TotalMeetsMinimum())
+            {
+                total -= 5.00;
+            }
+            Console.WriteLine($"Total Price:{FormatDecimal(total)}");
+
+            double GetDiscountPrice(int itemIndex)
+            {
+                return items[itemIndex] * (1 - discount[itemIndex]);
+            }
+            bool TotalMeetsMinimum()
+            {
+                return total >= minimumSpend;
+            }
+            string FormatDecimal(double input)
+            {
+                return input.ToString().Substring(0, 5);
+            }
+        }
+    }
+}
