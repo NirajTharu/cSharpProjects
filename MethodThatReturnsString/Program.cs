a7942662bdf035739424cf0d61d511b66fcf7662
NirajTharu <nirajc279@gmail.com>
2025-10-28 16:32:47 +0545

    Still need some modification

---
+++ MethodThatReturnsString/Program.cs
@@ -0,0 +1,25 @@
+﻿// Method that return the string
+using System;
+namespace StringMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This reverse a word
+            string input = "snake";
+            ReverseWord(input);
+            Console.WriteLine($"Reverse word is: {ReverseWord(input)}");
+            string ReverseWord(string word)
+            {
+                string result = "";
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    result += word[i];
+                }
+                return result;
+            }
+            string sentence = "";
+        }
+    }
+}
