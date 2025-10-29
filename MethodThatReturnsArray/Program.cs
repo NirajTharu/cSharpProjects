3e8a5b275aface211ba1af07e33df6ea528aaa46
NirajTharu <nirajc279@gmail.com>
2025-10-29 19:03:23 +0545

    Adding the file of the Method that returns The array

---
+++ MethodThatReturnsArray/Program.cs
@@ -0,0 +1,39 @@
+﻿// Method that returns the Array Value
+using System;
+using System.Security.Principal;
+using System.Transactions;
+namespace MethodReturnsArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int target = 60;
+            int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+            int[] result = TwoCoins(coins, target);
+            if (result.Length == 0)
+            {
+                Console.WriteLine("No two coins make change.");
+            }
+            else
+            {
+                Console.WriteLine($"Change found at positions {result[0]} and {result[1]}");
+            }
+
+            int[] TwoCoins(int[] coins, int target)
+            {
+                for (int curr = 0; curr < coins.Length; curr++)
+                {
+                    for (int next = curr + 1; next < coins.Length; next++)
+                    {
+                        if (coins[curr] + coins[next] == target)
+                        {
+                            return new int[] { curr, next };
+                        }
+                    }
+                }
+                return new int[0];
+            }
+        }
+    }
+}
