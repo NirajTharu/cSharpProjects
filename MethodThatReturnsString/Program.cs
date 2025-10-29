813350c07c42bb147037d5f36d27a446d7f45e69
NirajTharu <nirajc279@gmail.com>
2025-10-29 08:49:59 +0545

    Adding the file of the Method That returns the string with name change of the function according to the intended function

--- MethodThatReturnsString/Program.cs
+++ MethodThatReturnsString/Program.cs
@@ -8,20 +8,20 @@ namespace StringMethod
         {
             // This reverse a word
             string sentence = "there are snakes at the zoo";
-            Console.WriteLine($"Reverse word is: {ReverseWord(sentence)}");
-            Console.WriteLine($"The reverse sentnece is:{ReverseSentence(sentence)}");
+            Console.WriteLine($"Reverse sentence is: {ReverseString(sentence)}");
+            Console.WriteLine($"Each word are reversed: {ReverseEachWord(sentence)}");
 
-            string ReverseSentence(string sentence)
+            string ReverseEachWord(string sentence)
             {
                 string resultOfSentence = "";
                 string[] words = sentence.Split(" ");
                 foreach (string word in words)
                 {
-                    resultOfSentence += ReverseWord(word) + " ";
+                    resultOfSentence += ReverseString(word) + " ";
                 }
                 return resultOfSentence.Trim();
             }
-            string ReverseWord(string word)
+            string ReverseString(string word)
             {
                 string result = "";
                 for (int i = word.Length - 1; i >= 0; i--)
